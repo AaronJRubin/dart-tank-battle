b07d5f1bd4ac4dca082230431c254d9022d52413
Aaron J Rubin <aaronjrubin1@gmail.com>
2014-09-19 20:55:26 +0900

    Removed unnecessary file for migration of template from py to dart

--- pytodart.sh
+++
@@ -1,5 +0,0 @@
-#dev_appserver.py --port=9999 app-engine-site
-
-curl -o tank-battle-workspace/web/game-settings.html http://localhost:17080/bouncyballbattle/settings
-
-cp app-engine-site/static/main.css tank-battle-workspace/web/main.css
