aa42c060d811aea0108e5007f864756e4b3ee0b6
Aaron J Rubin <aaronjrubin1@gmail.com>
2014-09-23 08:06:42 +0900

    Implemented disabled button when no boxes checked

--- tank-battle-workspace/web/game_settings.dart
+++ tank-battle-workspace/web/game_settings.dart
@@ -1,150 +1,147 @@
 import 'dart:html';
 import 'stage.dart';
-import 'package:three/extras/image_utils.dart';
 import 'dart:convert';
 
+List<InputElement> playerCheckboxes = ["One", "Two", "Three", "Four"].map((id) => document.querySelector("#" + id + "-use")).toList(growable: false);
+
+InputElement getPlayerCheckbox(String id) {
+  return playerCheckboxes.where((checkbox) => checkbox.id == id + "-use").first;
+}
+
+bool isSelected(String playerID) {
+  InputElement checkbox = getPlayerCheckbox(playerID);
+  return checkbox.checked;
+}
+
+List<InputElement> selectedCheckboxes() {
+  return ["One", "Two", "Three", "Four"].where(isSelected).toList(growable: false);
+}
+
+
+ButtonElement playButton = document.querySelector(".play-button");
+
 main() {
- /* preloadTextures(); */
-  ButtonElement playButton = document.querySelector(".play-button");
+  playerCheckboxes.forEach((checkbox) {
+    checkbox.onClick.listen((onData) {
+      if (selectedCheckboxes().length == 0) {
+        playButton.disabled = true;
+      } else {
+        playButton.disabled = false;
+      }
+    });
+  });
   playButton.onClick.listen((event) => play());
-  InputElement playerOneCheckbox = document.querySelector("#One-use");
+  InputElement playerOneCheckbox = getPlayerCheckbox("One");
   playerOneCheckbox.checked = true;
-  InputElement playerTwoCheckbox = document.querySelector("#Two-use");
+  InputElement playerTwoCheckbox = getPlayerCheckbox("Two");
   playerTwoCheckbox.checked = true;
 }
 
-/* Load these into the browser cache before the game begins,
- * so there aren't any weirdly untextured things during gameplay!
- * This is necessary because static initializers in Dart (which
- * are used to load textures - static instance variables -
- * in classes such as Player) are evaluated lazily, at first read rather
- * than at class load time. This means that the game can start
- * playing before all textures have been loaded, and that's no good.
- * For more information about the lazy initialization of statics,
- * see https://groups.google.com/a/dartlang.org/forum/#!topic/misc/dKurFjODRXQ
-   */
-
-void preloadTextures() {
-  loadTexture('lava-stage-textures/boiled_flesh.jpg');
-  loadTexture('lava-stage-textures/lava.jpg');
-  loadTexture('lava-stage-textures/Lava_texture_by_Twister10.jpg');
-  loadTexture('lava-stage-textures/william_wall_01_S.png');
-  loadTexture('nine-pillar-stage-textures/particleenergyball-ss-alleffects.png');
-  loadTexture('nine-pillar-stage-textures/rsz_1cloud--texture-3.jpg');
-  loadTexture('night-sky/nightsky_back.jpg');
-  loadTexture('night-sky/nightsky_front.jpg');
-  loadTexture('night-sky/nightsky_left.jpg');
-  loadTexture('night-sky/nightsky_right.jpg');
-  loadTexture('night-sky/nightsky_top.jpg');
-  loadTexture('general-purpose-textures/plain_white.jpg');
+int stringToKeyCode(String input) {
+  switch (input) {
+    case 'A':
+      return KeyCode.A;
+    case 'B':
+      return KeyCode.B;
+    case 'C':
+      return KeyCode.C;
+    case 'D':
+      return KeyCode.D;
+    case 'E':
+      return KeyCode.E;
+    case 'F':
+      return KeyCode.F;
+    case 'G':
+      return KeyCode.G;
+    case 'H':
+      return KeyCode.H;
+    case 'I':
+      return KeyCode.I;
+    case 'J':
+      return KeyCode.J;
+    case 'K':
+      return KeyCode.K;
+    case 'L':
+      return KeyCode.L;
+    case 'M':
+      return KeyCode.M;
+    case 'N':
+      return KeyCode.N;
+    case 'O':
+      return KeyCode.O;
+    case 'P':
+      return KeyCode.P;
+    case 'Q':
+      return KeyCode.Q;
+    case 'R':
+      return KeyCode.R;
+    case 'S':
+      return KeyCode.S;
+    case 'T':
+      return KeyCode.T;
+    case 'U':
+      return KeyCode.U;
+    case 'V':
+      return KeyCode.V;
+    case 'W':
+      return KeyCode.W;
+    case 'X':
+      return KeyCode.X;
+    case 'Y':
+      return KeyCode.Y;
+    case 'Z':
+      return KeyCode.Z;
+    case '1':
+      return KeyCode.ONE;
+    case '2':
+      return KeyCode.TWO;
+    case '3':
+      return KeyCode.THREE;
+    case '4':
+      return KeyCode.FOUR;
+    case '5':
+      return KeyCode.FIVE;
+    case '6':
+      return KeyCode.SIX;
+    case '7':
+      return KeyCode.SEVEN;
+    case '8':
+      return KeyCode.EIGHT;
+    case '9':
+      return KeyCode.NINE;
+    case '0':
+      return KeyCode.NINE;
+    case 'LEFT':
+      return KeyCode.LEFT;
+    case 'RIGHT':
+      return KeyCode.RIGHT;
+    case 'UP':
+      return KeyCode.UP;
+    case 'DOWN':
+      return KeyCode.DOWN;
+    default:
+      return KeyCode.UNKNOWN;
+  }
 }
 
-
- int stringToKeyCode(String input) {
-    switch(input) {
-      case 'A':
-        return KeyCode.A;
-      case 'B':
-        return KeyCode.B;
-      case 'C':
-        return KeyCode.C;
-      case 'D':
-        return KeyCode.D;
-      case 'E':
-        return KeyCode.E;
-      case 'F':
-        return KeyCode.F;
-      case 'G':
-        return KeyCode.G;
-      case 'H':
-        return KeyCode.H;
-      case 'I':
-        return KeyCode.I;
-      case 'J':
-        return KeyCode.J;
-      case 'K':
-        return KeyCode.K;
-      case 'L':
-        return KeyCode.L;
-      case 'M':
-        return KeyCode.M;
-      case 'N':
-        return KeyCode.N;
-      case 'O':
-        return KeyCode.O;
-      case 'P':
-        return KeyCode.P;
-      case 'Q':
-        return KeyCode.Q;
-      case 'R':
-        return KeyCode.R;
-      case 'S':
-        return KeyCode.S;
-      case 'T':
-        return KeyCode.T;
-      case 'U':
-        return KeyCode.U;
-      case 'V':
-        return KeyCode.V;
-      case 'W':
-        return KeyCode.W;
-      case 'X':
-        return KeyCode.X;
-      case 'Y':
-        return KeyCode.Y;
-      case 'Z':
-        return KeyCode.Z;
-      case '1':
-        return KeyCode.ONE;
-      case '2':
-        return KeyCode.TWO;
-      case '3':
-        return KeyCode.THREE;
-      case '4':
-        return KeyCode.FOUR;
-      case '5':
-        return KeyCode.FIVE;
-      case '6':
-        return KeyCode.SIX;
-      case '7':
-        return KeyCode.SEVEN;
-      case '8':
-        return KeyCode.EIGHT;
-      case '9':
-        return KeyCode.NINE;
-      case '0':
-        return KeyCode.NINE;
-      case 'LEFT':
-        return KeyCode.LEFT;
-      case 'RIGHT':
-        return KeyCode.RIGHT;
-      case 'UP':
-        return KeyCode.UP;
-      case 'DOWN':
-        return KeyCode.DOWN;
-      default:
-        return KeyCode.UNKNOWN;
-    }
+double stringToHue(String input) {
+  switch (input) {
+    case 'RED':
+      return 1.0;
+    case 'BLUE':
+      return 251 / 360;
+    case 'GREEN':
+      return 132 / 360;
+    case 'ORANGE':
+      return 31 / 360;
+    case 'YELLOW':
+      return 59 / 360;
+    case 'VIOLET':
+      return 293 / 360;
+    default:
+      return 0.0;
   }
-
- double stringToHue(String input) {
-      switch (input) {
-        case 'RED':
-          return 1.0;
-        case 'BLUE':
-          return 251 / 360;
-        case 'GREEN':
-          return 132 / 360;
-        case 'ORANGE':
-          return 31 / 360;
-        case 'YELLOW':
-          return 59 / 360;
-        case 'VIOLET':
-          return 293 / 360;
-        default:
-          return 0.0;
-      }
-    }
+}
 
 Map getPlayerMap(String playerID) {
   SelectElement leftKeySelector = document.querySelector("#" + playerID + "-rotate-left");
@@ -157,46 +154,50 @@ Map getPlayerMap(String playerID) {
   int reverseKey = stringToKeyCode(reverseKeySelector.selectedOptions.first.value);
   SelectElement colorSelector = document.querySelector("#" + playerID + "-color");
   double hue = stringToHue(colorSelector.selectedOptions.first.value);
-  return {'left' : leftKey, 'right' : rightKey, 'accelerate' : accelerateKey, 'reverse' : reverseKey, 'hue' : hue, 'name' : 'Player' + playerID};
+  return {
+    'left': leftKey,
+    'right': rightKey,
+    'accelerate': accelerateKey,
+    'reverse': reverseKey,
+    'hue': hue,
+    'name': 'Player' + playerID
+  };
 }
 
-bool isSelected(String playerID) {
-  InputElement checkbox = document.querySelector("#" + playerID + "-use");
-  return checkbox.checked;
-}
+
+
 
 void play() {
-  print("Play function has been called!");
   List<Map> playerMaps = ["One", "Two", "Three", "Four"].where(isSelected).map(getPlayerMap).toList();
   Map settings = {};
   settings['players'] = playerMaps;
   SelectElement stageSelect = document.querySelector("#stage-select");
-    String selectedStageName = stageSelect.selectedOptions[0].value;
-    int selectedStage;
-    switch(selectedStageName) {
-      case ('basic-stage'):
-        selectedStage = Stage.BASICSTAGE;
-        break;
-      case('nine-pillar-stage'):
-        selectedStage = Stage.NINEPILLARSTAGE;
-        break;
-      case('nine-pillar-stage-mobile'):
-        selectedStage = Stage.MOVINGNINEPILLARSTAGE;
-        break;
-      case('lava-walls'):
-          selectedStage = Stage.LAVADEATHSTAGE;
-          break;
-      case ('lava-no-walls'):
-        selectedStage = Stage.LAVADEATHSTAGENOWALLS;
-        break;
-      default:
-        selectedStage = Stage.BASICSTAGE;
-    }
-    settings['stage'] = selectedStage;
-    Storage localStorage = window.localStorage;
-    String settingsString = JSON.encode(settings);
-   // print("The following settingsString was generated " + settingsString);
-    localStorage['settings'] = settingsString;
-    print("About to redirect!");
-    window.location.assign("bouncy-ball-battle.html");
+  String selectedStageName = stageSelect.selectedOptions[0].value;
+  int selectedStage;
+  switch (selectedStageName) {
+    case ('basic-stage'):
+      selectedStage = Stage.BASICSTAGE;
+      break;
+    case ('nine-pillar-stage'):
+      selectedStage = Stage.NINEPILLARSTAGE;
+      break;
+    case ('nine-pillar-stage-mobile'):
+      selectedStage = Stage.MOVINGNINEPILLARSTAGE;
+      break;
+    case ('lava-walls'):
+      selectedStage = Stage.LAVADEATHSTAGE;
+      break;
+    case ('lava-no-walls'):
+      selectedStage = Stage.LAVADEATHSTAGENOWALLS;
+      break;
+    default:
+      selectedStage = Stage.BASICSTAGE;
+  }
+  settings['stage'] = selectedStage;
+  Storage localStorage = window.localStorage;
+  String settingsString = JSON.encode(settings);
+  // print("The following settingsString was generated " + settingsString);
+  localStorage['settings'] = settingsString;
+  print("About to redirect!");
+  window.location.assign("bouncy-ball-battle.html");
 }
